24366867e24e00ee1193083bb914e6c09b1ebe31
pratik chindhe <169028142+TxPrtaik@users.noreply.github.com>
2025-01-04 13:33:22 +0530

    qwerty

--- routes/connection.js
+++ routes/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 var util = require('util')
 
 var conn = mysql.createConnection({
-    host: "bun9fq9ycki9avlxsm6h-mysql.services.clever-cloud.com ",
+    host: "bun9fq9ycki9avlxsm6h-mysql.services.clever-cloud.com",
     user: "uhfctuxbu0ljcj9m",
     password: "s97lFinSkHJXw8Mmwq2c",
     database: "bun9fq9ycki9avlxsm6h"
--- routes/master.js
+++ routes/master.js
@@ -3,7 +3,7 @@ let route=express.Router();
 let mysql=require('mysql');
 let exe=require('./connection')
 function validateAdmin(req,res,next){
-
+    // req.session.mid=1;
     if(req.session.mid){
 next();
 }
@@ -11,12 +11,21 @@ else{
 res.render('master/login.ejs');
 }
 }
+
 route.get("/",validateAdmin,async(req,res)=>{
-res.render("master/index.ejs");
+    let user=await exe(`select*from admin where id='${req.session.mid}'`);
+    let obj={
+    "admin": user[0]
+   }
+res.render("master/index.ejs",obj);
 });
 
 route.get("/add-admin",validateAdmin,async(req,res)=>{
-res.render("master/addadmin.ejs");
+    let user=await exe(`select*from admin where id='${req.session.mid}'`);
+    let obj={
+    "admin": user[0]
+   }
+res.render("master/addadmin.ejs",obj);
 });
 route.post("/save-admin",validateAdmin,async(req,res)=>{
     req.body.image=new Date().getTime()+req.files.img.name;
@@ -26,14 +35,54 @@ res.redirect("/add-admin");
 })
 route.get("/admin-list",validateAdmin,async(req,res)=>{
     let d=await exe('select*from admin');
+    let user=await exe(`select*from admin where id='${req.session.mid}'`);
+   
     let obj={
-        "data":d
+        "data":d,
+        "admin":user[0]
     }
     res.render("master/adminlist.ejs",obj);
 })
 
 
 route.post("/admin-login",async(req,res)=>{
-    res.send(req.body);
+   let d=await exe(`select*from admin where contact='${req.body.number}' and password='${req.body.pass}'`);
+   if(d.length!=0){
+    req.session.mid=d[0].id;
+  res.send("1");
+   }else{
+   res.send("0");
+   }
+})
+route.get("/admin-profile",validateAdmin,async(req,res)=>{
+    let user=await exe(`select*from admin where id='${req.session.mid}'`);
+   
+    let obj={
+        
+        "admin":user[0]
+    }
+    res.render("master/admindetails.ejs",obj);
+})
+route.post("/update-admin/:id",async(req,res)=>{
+if(req.files){
+req.body.image=new Date().getTime()+req.files.img.name;
+req.files.img.mv("public/images/"+req.body.image);
+let d=await exe(`update admin set name='${req.body.name}',contact='${req.body.number}',image='${req.body.image}',password='${req.body.pass}' where id='${req.params.id}'`);
+}
+else{
+    let d=await exe(`update admin set name='${req.body.name}',contact='${req.body.number}',password='${req.body.pass}' where id='${req.params.id}'`);
+
+}
+
+res.redirect("/admin-profile");
+})
+route.get("/logout",validateAdmin,async(req,res)=>{
+    req.session.mid=undefined;
+    res.redirect("/");
+})
+route.get("/delete-admin/:id",validateAdmin,async(req,res)=>{
+let d=await exe(`delete from admin where id='${req.params.id}'`);
+req.session.mid=undefined;
+res.redirect("/");
 })
 module.exports=route;
