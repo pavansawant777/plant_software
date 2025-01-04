482d41ebe89f8c13306f9496d19e74fe606bee90
pratik chindhe <169028142+TxPrtaik@users.noreply.github.com>
2025-01-04 10:59:03 +0530

    qwer

--- routes/master.js
+++ routes/master.js
@@ -1,6 +1,18 @@
 let express=require('express');
 let route=express.Router();
-route.get("/",async(req,res)=>{
+function validateAdmin(req,res,next){
+req.session.mid=1;
+    if(req.session.mid){
+next();
+}
+else{
+res.render('master/login.ejs');
+}
+}
+route.get("/",validateAdmin,async(req,res)=>{
 res.render("master/index.ejs");
+});
+route.get("/add-admin",validateAdmin,async(req,res)=>{
+res.render("master/addadmin.ejs");
 });
 module.exports=route;
